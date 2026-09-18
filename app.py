9eca20426f3c6bea987471372d5164474a7504bf
sebastianccala04 <sebastianccala3@gmail.com>
2026-09-18 01:10:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,19 @@
 import streamlit as st
 
 # Configuración de la página
-st.set_page_config(
-    page_title="Proyecto Python Fundamentals",
-    page_icon="🐍",
-    layout="wide"
-)
+st.set_page_config(page_title="Proyecto Python Fundamentals",
+    layout="wide")
 
 # Menú lateral
 st.sidebar.title("Menú principal")
 
 opcion = st.sidebar.selectbox(
     "Selecciona una sección:",
-    [
-        "Home",
-        "Ejercicio 1",
-        "Ejercicio 2",
-        "Ejercicio 3",
-        "Ejercicio 4"
-    ]
-)
+    ["Home",
+    "Ejercicio 1",
+    "Ejercicio 2",
+    "Ejercicio 3",
+    "Ejercicio 4"])
 
 
 # ==========================
