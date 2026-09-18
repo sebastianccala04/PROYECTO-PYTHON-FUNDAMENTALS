55f23f70eff4c45fb431a219243c99e75d3e78da
sebastianccala04 <sebastianccala3@gmail.com>
2026-09-18 00:52:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
 import streamlit as st
-st.title("Especializacion Python for Analytics")
-st.sidebar.title("Parámetros")
-st.write("Elaborado por: Sebastian Ccala")
+
+# Configuración de la página
+st.set_page_config(
+    page_title="Proyecto Python Fundamentals",
+    page_icon="🐍",
+    layout="wide")
+
+# Menú lateral
+st.sidebar.title("Menú principal")
+
+opcion = st.sidebar.selectbox(
+    "Selecciona una sección:",
+    ["Home",
+    "Ejercicio 1",
+    "Ejercicio 2",
+    "Ejercicio 3",
+    "Ejercicio 4"])
