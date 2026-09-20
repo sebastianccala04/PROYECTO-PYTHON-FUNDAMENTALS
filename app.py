eea8ba0fceb5765905fab59b78d5194ffa37621b
sebastianccala04 <sebastianccala3@gmail.com>
2026-09-20 02:34:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ if "inventario" not in st.session_state:
 # MENÚ LATERAL
 # ============================================================
 
-st.sidebar.image("LOGO 4.png")
+st.sidebar.image("LOGO 2.png",width =100)
 
 st.sidebar.title("📚Menú")
 pagina = st.sidebar.selectbox(
